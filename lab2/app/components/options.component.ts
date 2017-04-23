bb22fd6c805a29a15b70d23fc3ccdf8c5e06a46d
Sami2205 <samlik@gmx.net>
2017-04-23 20:59:08 +0200

    passwort bearbeiten verbessert

--- lab2/app/components/options.component.ts
+++ lab2/app/components/options.component.ts
@@ -1,6 +1,29 @@
 import { Component } from '@angular/core';
+
 @Component({
     selector: 'options',
     templateUrl: '/app/views/options.html'
 })
-export class OptionsComponent {}
+export class OptionsComponent {
+    patternOld = /.{4,12}/
+    patternNew = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}/;
+    oldInput: string = "";
+    newInput: string = "";
+    confirmInput: string = "";
+
+    formValidation(): boolean{
+        if(!this.patternOld.test(this.oldInput)){
+            return false;
+        }else if(!this.patternNew.test(this.newInput)){
+            return false;
+        }else if((this.confirmInput !== this.newInput)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    submitForm(): void{
+        this.oldInput = this.newInput = this.confirmInput = "";
+    }
+}
